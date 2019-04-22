0f7e180aab8adf3d24b282c6096c2908de677719
ronn <ronnie-filth@hotmail.com>
2019-04-23 00:06:49 +0200

    Build interconection for each second flight

--- src/main/java/com/ryan/flights/FlightsController.java
+++ src/main/java/com/ryan/flights/FlightsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -35,69 +36,139 @@ public class FlightsController {
     }
 
     @RequestMapping("/interconnections")
-    public List<Route> prueba(
+    public List<Interconnection> prueba(
                                          @RequestParam("departure") String departureAirport,
                                          @RequestParam("arrival") String arrivalAirport,
                                          @RequestParam("departureDateTime") String departureDateTimeStr,
                                          @RequestParam("arrivalDateTime") String arrivalDateTimeStr
     ){
-        return routesService.getValidRoutes(departureAirport, arrivalAirport);
-
- /*       if (getValidRoutes(departureAirport, arrivalAirport).isEmpty()){
-            return new ResponseEntity<>(
-                    "Ruta no válida",
-                    HttpStatus.FORBIDDEN
-            );
-        }
+        List<Route> validRoutes = routesService.getValidRoutes(departureAirport, arrivalAirport);
 
         LocalDateTime departureDateTime = LocalDateTime.parse(departureDateTimeStr);
-
-        Schedule schedule = schedulesService.getSchedule(departureAirport, arrivalAirport, departureDateTime);
-
         LocalDateTime arrivalDateTime = LocalDateTime.parse(arrivalDateTimeStr);
 
-        return new ResponseEntity<>(
-                getInterconnections(departureAirport, arrivalAirport, schedule, departureDateTime, arrivalDateTime),
-                HttpStatus.OK
-        );*/
+        List<Leg> legs = validRoutes.stream()
+                .flatMap(route -> schedulesService.getSchedule(route.getAirportFrom(), route.getAirportTo(), departureDateTime)
+                        .map(schedule -> getValidDaysFirstLeg(schedule, departureDateTime)
+                                .stream()
+                                .flatMap(day -> day.getFlights()
+                                        .stream()
+                                        .map(flight -> new Leg(
+                                                route.getAirportFrom(),
+                                                route.getAirportTo(),
+                                                getOf(
+                                                        day,
+                                                        schedule.getMonth(),
+                                                        departureDateTime.getYear(),
+                                                        flight.getDepartureTime()
+                                                ).toString(),
+                                                getOf(
+                                                        day,
+                                                        schedule.getMonth(),
+                                                        departureDateTime.getYear(),
+                                                        flight.getArrivalTime()
+                                                ).toString()
+                                                )
+                                        )
+                                )
+                        ).orElse(null)
+                ).collect(toList());
+
+        List<Interconnection> collect = legs.stream()
+                //TODO consultar si la ruta existe primero...
+                .flatMap(leg -> schedulesService.getSchedule(leg.getArrivalAirport(), arrivalAirport, departureDateTime)
+                        .map(schedule -> getValidDaysSecondLeg(schedule, leg, arrivalDateTime)// TODO aquí tomar validDays para 2ds stop
+                                .stream()
+                                .flatMap(day -> day.getFlights()
+                                        .stream()
+                                        .map(flight -> {
+
+                                                    Leg leg1 = new Leg(
+                                                            leg.getArrivalAirport(),
+                                                            arrivalAirport,
+                                                            getOf(
+                                                                    day,
+                                                                    schedule.getMonth(),
+                                                                    departureDateTime.getYear(),
+                                                                    flight.getDepartureTime()
+                                                            ).toString(),
+                                                            getOf(
+                                                                    day,
+                                                                    schedule.getMonth(),
+                                                                    departureDateTime.getYear(),
+                                                                    flight.getArrivalTime()
+                                                            ).toString()
+                                                    );
+                                                    return new Interconnection(Arrays.asList(leg, leg1));
+                                                }
+                                        )
+                                )
+                        )
+                        .orElse(null)
+                ).collect(toList());
+        return collect;
     }
 
     private List<Interconnection> getInterconnections(String departureAirport, String arrivalAirport, Schedule schedule, LocalDateTime departureDateTime, LocalDateTime arrivalDateTime) {
-        return getValidDays(schedule, departureDateTime, arrivalDateTime.toLocalTime())
+        return getValidDaysFirstLeg(schedule, departureDateTime)
                 .stream()
                 .map(day -> build(departureAirport, arrivalAirport, day, schedule.getMonth(), departureDateTime.getYear()))
                 .collect(toList());
     }
 
 
-    private List<Day> getValidDays(Schedule schedule, LocalDateTime departure, LocalTime arrival) {
+    private List<Day> getValidDaysFirstLeg(Schedule schedule, LocalDateTime departure) {
         return schedule
                 .getDays()
                 .stream()
                 .filter(day -> day.getDay().equals(departure.getDayOfMonth()))
-                .filter(getValidDay(departure.toLocalTime(), arrival))
+                .filter(getValidDayFirstLeg(departure.toLocalTime()))
                 .collect(toList());
     }
 
-    private Predicate<Day> getValidDay(LocalTime departure, LocalTime arrival) {
+    private List<Day> getValidDaysSecondLeg(Schedule schedule, Leg firstLeg, LocalDateTime arrivalDateTime) {
+        LocalDateTime arrivalDateTimeFirstLeg = LocalDateTime.parse(firstLeg.getArrivalDateTime());
+        return schedule
+                .getDays()
+                .stream()
+                .filter(day -> day.getDay().equals(arrivalDateTimeFirstLeg.getDayOfMonth()))
+                .filter(getValidDaySecondLeg(arrivalDateTimeFirstLeg.toLocalTime(), arrivalDateTime.toLocalTime()))
+                .collect(toList());
+    }
+
+    private Predicate<Day> getValidDayFirstLeg(LocalTime departure) {
         return day -> day.getFlights()
                 .stream()
-                .anyMatch(getValidFlight(departure, arrival)
+                .anyMatch(getValidFlightByDepartureTime(departure)
                 );
     }
 
-    private Predicate<Flight> getValidFlight(LocalTime departure, LocalTime arrival) {
-        return flight ->
-                !flight.getDepartureTime().isBefore(departure)
-                &&
-                !flight.getArrivalTime().isAfter(arrival);
+    private Predicate<Day> getValidDaySecondLeg(LocalTime arrivalDateTimeFirstLeg, LocalTime arrivalEntireTrip) {
+        return day -> day.getFlights()
+                .stream()
+                .anyMatch(getValidFlightByArrivalTime(arrivalDateTimeFirstLeg, arrivalEntireTrip));
     }
 
+    private Predicate<Flight> getValidFlightByDepartureTime(LocalTime departure) {
+        return flight ->
+                !flight.getDepartureTime().isBefore(departure);
+    }
 
+    private Predicate<Flight> getValidFlightByArrivalTime(LocalTime arrivalTimeFirstLeg, LocalTime arrivalEntireTrip) {
+        return flight -> {
+            if (arrivalTimeFirstLeg.getHour() < 22){
+                System.out.println(arrivalTimeFirstLeg + " :: " + flight.getDepartureTime() + " => " + (arrivalTimeFirstLeg.plusHours(2).isBefore(flight.getDepartureTime()) &&
+                        !flight.getArrivalTime().isAfter(arrivalEntireTrip)));
+                return arrivalTimeFirstLeg.plusHours(2).isBefore(flight.getDepartureTime())
+                        &&
+                        !flight.getArrivalTime().isAfter(arrivalEntireTrip);
+            }
+            return false;
+        };
+    }
 
     private Interconnection build(String departureAirport, String arrivalAirport, Day day, Integer month, Integer year){
         return new Interconnection(
-                day.getFlights().size() - 1,
                 day.getFlights().stream()
                         .map(flight -> buildLeg(
                                 departureAirport,
