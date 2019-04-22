445d07b157fdad019f5518226be3c592d2345079
ronn <ronnie-filth@hotmail.com>
2019-04-23 00:05:50 +0200

    Return Optional instead of possible null

--- src/main/java/com/ryan/flights/infrastructure/acl/schedules/SchedulesService.java
+++ src/main/java/com/ryan/flights/infrastructure/acl/schedules/SchedulesService.java
@@ -3,6 +3,7 @@ package com.ryan.flights.infrastructure.acl.schedules;
 import com.ryan.flights.infrastructure.acl.ConsumerService;
 import com.ryan.flights.infrastructure.acl.schedules.model.Schedule;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.time.LocalDateTime;
 
@@ -16,7 +17,12 @@ public class SchedulesService {
     }
 
     public Schedule getSchedule(String departure, String arrival, LocalDateTime departureDateTime){
-        return consumerService.getSchedule(getUrlSche(departure, arrival, departureDateTime));
+        try {
+            return consumerService.getSchedule(getUrlSche(departure, arrival, departureDateTime));
+        }catch (HttpClientErrorException httpcee){
+            System.out.println("Schedule for " + departure + " -> " + arrival + " Not found");
+            return null;
+        }
     }
 
     private String getUrlSche(String departure, String arrival, LocalDateTime departureDateTime) {
