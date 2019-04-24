603a12698218985cd6324394c3853d271d72c4d1
ronn <ronnie-filth@hotmail.com>
2019-04-24 21:06:51 +0200

    HttpServerError catched

--- src/main/java/com/ryan/flights/infrastructure/acl/schedules/ScheduleConsumer.java
+++ src/main/java/com/ryan/flights/infrastructure/acl/schedules/ScheduleConsumer.java
@@ -2,18 +2,21 @@ package com.ryan.flights.infrastructure.acl.schedules;
 
 import com.ryan.flights.infrastructure.acl.ConsumerService;
 import com.ryan.flights.infrastructure.acl.schedules.model.Schedule;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
-public class SchedulesService {
+public class ScheduleConsumer {
 
     private final ConsumerService consumerService;
+    private static final Logger LOGGER = Logger.getLogger(ScheduleConsumer.class);
 
-    public SchedulesService(ConsumerService consumerService) {
+    public ScheduleConsumer(ConsumerService consumerService) {
         this.consumerService = consumerService;
     }
 
@@ -21,7 +24,10 @@ public class SchedulesService {
         try {
             return Optional.ofNullable(consumerService.getSchedule(getUrlSche(departure, arrival, departureDateTime)));
         }catch (HttpClientErrorException httpcee){
-            System.out.println("Schedule for " + departure + " -> " + arrival + " Not found");
+            LOGGER.info("Schedule for " + departure + " -> " + arrival + " Not found");
+            return Optional.empty();
+        }catch (HttpServerErrorException httpsee){
+            LOGGER.error("ScheduleService not available, try later");
             return Optional.empty();
         }
     }
