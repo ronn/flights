67b433bda3d66a5b3079ca1d2328314add8b0581
ronn <ronnie-filth@hotmail.com>
2019-05-11 21:03:19 +0200

    Add vavrModule to des-serialize vavr Objects

--- src/main/java/com/ryan/flights/FlightsApplication.java
+++ src/main/java/com/ryan/flights/FlightsApplication.java
@@ -1,9 +1,12 @@
 package com.ryan.flights;
 
+import com.fasterxml.jackson.databind.Module;
+import io.vavr.jackson.datatype.VavrModule;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -12,5 +15,10 @@ public class FlightsApplication {
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
 		SpringApplication.run(FlightsApplication.class, args);
+	}
+
+	@Bean
+	Module vavrModule(){
+		return new VavrModule();
 	}
 }
